7d44d672bf4284e9003bdfa4907d56311256ab7c
Himmat Solanki <himmat.solanki@gmail.com>
2021-04-18 18:00:02 +0100

    First commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+
+        [Fact]
+        public void GetDateByAddingIndex()
+        {
+            var item = controller.GetById(1);
+
+            Assert.Equal(DateTime.Now.AddDays(1).Date, item.Date.Date);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
